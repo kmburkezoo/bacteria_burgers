c216cda44b26be6c5077ecd4f09f3fbe370b2d2f
Katelin Burke <katelinburke@gmail.com>
2022-03-11 16:21:08 -0700

    bubble chart!

--- charts.js
+++ charts.js
@@ -87,15 +87,43 @@ function buildCharts(sample) {
         x:sampleValues.slice(0,10),
         y:yticks,
         type:"bar",
-        orientation:"h"
+        orientation:"h",
+        text:otuLabels
     }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     title:"Top 10 Bacteria Cultures Found"/*,
-     yaxis:{label:otuLabels}*/
+     title:"Top 10 Bacteria Cultures Found",
+     text:otuLabels
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar",barData,barLayout)
+    Plotly.newPlot("bar", barData, barLayout)
+
+    // Bubble chart
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x:otuIDs,
+      y:sampleValues,
+      text:otuLabels,
+      mode: "markers",
+      marker:{
+        size:sampleValues,
+        color:otuIDs
+        // colorscale:
+      },
+      text:otuLabels,
+    }];
+    console.log(bubbleData)
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title:"Bacteria Cultures Per Sample",
+      xaxis:{title:"OTU ID"},
+      hovermode: "closest",
+      automargin: true     
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
   });
 }
-
